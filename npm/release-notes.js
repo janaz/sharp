a6d3faab1ac02e0c9b99522dafa12a43ba50fa62
Tomasz Janowski <janaz9@gmail.com>
2026-02-03 13:57:19 +0000

    changelog for revizly

--- npm/release-notes.js
+++ npm/release-notes.js
@@ -1,7 +1,7 @@
 const { readFileSync, writeFileSync } = require('node:fs');
 
 const { version } = require('./package.json');
-const versionWithoutPreRelease = version.replace(/-rc\.\d+$/, '');
+const versionWithoutPreRelease = version.replace(/-revizly\d+$/, '');
 
 const markdown = readFileSync(`./docs/src/content/docs/changelog/v${versionWithoutPreRelease}.md`, 'utf8');
 const markdownWithoutFrontmatter = markdown.replace(/---\n.*?\n---\n+/s, '');
