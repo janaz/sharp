2eb8035c2158fb70c087fae5c50f99ad0a39b9bf
Tomasz Janowski <janaz9@gmail.com>
2026-04-07 03:56:46 +0000

    release

--- npm/wasm-wrappers.js
+++ npm/wasm-wrappers.js
@@ -8,10 +8,7 @@
 const { copyFileSync, readFileSync, writeFileSync } = require('node:fs');
 const { join } = require('node:path');
 
-const platforms = [
-  'freebsd-wasm32',
-  'webcontainers-wasm32'
-];
+const platforms = [];
 
 const readme = readFileSync(join(__dirname, '..', 'README.md'), 'utf8');
 const licensing = readme.substring(readme.indexOf('## Licensing'));
