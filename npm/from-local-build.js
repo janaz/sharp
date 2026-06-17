4be573ef5b1f45097a6cd1a1fe27b238cf57732f
Tomasz Janowski <janaz9@gmail.com>
2026-06-17 22:58:16 +0000

    release prep

--- npm/from-local-build.js
+++ npm/from-local-build.js
@@ -5,7 +5,7 @@
 
 // Populate the npm package for the current platform with the local build
 
-const { copyFileSync, cpSync, readFileSync, writeFileSync, appendFileSync } = require('node:fs');
+const { copyFileSync, cpSync, readFileSync, writeFileSync } = require('node:fs');
 const { basename, join } = require('node:path');
 
 const { buildPlatformArch } = require('../dist/libvips.cjs');
@@ -40,15 +40,3 @@ writeFileSync(join(destDir, 'index.cjs'), `${requireStub}module.exports = requir
 
 // Copy Apache-2.0 LICENSE
 copyFileSync(join(__dirname, '..', 'LICENSE'), join(destDir, 'LICENSE'));
-
-// Copy files for packages without an explicit sharp-libvips dependency (Windows, wasm)
-if (platform.startsWith('win') || platform.startsWith('wasm')) {
-  const libvipsPlatform = platform === 'wasm32' ? 'dev-wasm32' : platform;
-  const sharpLibvipsDir = join(require(`@img/sharp-libvips-${libvipsPlatform}/lib`), '..');
-  // Copy versions.json
-  copyFileSync(join(sharpLibvipsDir, 'versions.json'), join(destDir, 'versions.json'));
-  // Append third party licensing to README
-  const libvipsReadme = readFileSync(join(sharpLibvipsDir, 'README.md'), { encoding: 'utf-8' });
-  const thirdParty = libvipsReadme.substring(libvipsReadme.indexOf('\nThis software contains'));
-  appendFileSync(join(destDir, 'README.md'), thirdParty);
-}
